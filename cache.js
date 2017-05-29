0d2c0658d01ca0c706581001ceffc9e1310de655
Damon Oehlman <damon.oehlman@gmail.com>
2017-05-29 17:32:03 +1000

    apply eslint autofixes

--- cache.js
+++ cache.js
@@ -1,11 +1,11 @@
 /* jshint node: true */
-'use strict';
 
-var mkdirp = require('mkdirp');
-var path = require('path');
-var fs = require('fs');
-var reCharset = /^.*charset\=(.*)$/;
-var reInvalidCacheChars = /(\:\/+|\/+|\.(?!\w+$))/g;
+
+const mkdirp = require('mkdirp');
+const path = require('path');
+const fs = require('fs');
+const reCharset = /^.*charset\=(.*)$/;
+const reInvalidCacheChars = /(\:\/+|\/+|\.(?!\w+$))/g;
 
 /**
 ## getit cache helpers
@@ -13,40 +13,39 @@ var reInvalidCacheChars = /(\:\/+|\/+|\.(?!\w+$))/g;
 ### cache.get(target, opts, callback)
 **/
 exports.get = function(target, opts, callback) {
-  var cacheData = {};
-  var cacheFile;
-  var metaFile;
+  let cacheData = {};
+  let cacheFile;
+  let metaFile;
 
   // if we have no cache folder, then trigger the callback with no data
-  if (! opts.cachePath) {
+  if (!opts.cachePath) {
     callback(cacheData);
   }
   // otherwise, look for an etag file
   else {
     cacheFile = path.resolve(opts.cachePath, getCacheTarget(target));
-    metaFile = cacheFile + '.meta';
-          
+    metaFile = `${cacheFile}.meta`;
+
     // read the etag file
-    fs.readFile(metaFile, 'utf8', function(err, data) {
-      var match;
-      var encoding;
-      
-      if (! err) {
+    fs.readFile(metaFile, 'utf8', (err, data) => {
+      let match;
+      let encoding;
+
+      if (!err) {
         cacheData = JSON.parse(data);
-        
+
         // look for an encoding specification in the metadata
         match = reCharset.exec(cacheData['content-type']);
         encoding = match ? match[1] : 'utf8';
-        
-        fs.readFile(cacheFile, encoding, function(err, data) {
-          if (! err) {
+
+        fs.readFile(cacheFile, encoding, (err, data) => {
+          if (!err) {
             cacheData.data = data;
           }
-          
+
           callback(cacheData);
         });
-      }
-      else {
+      } else {
         callback(cacheData);
       }
     });
@@ -57,32 +56,32 @@ exports.get = function(target, opts, callback) {
 ### cache.update(target, opts, resErr, res, body, callback)
 **/
 exports.update = function(target, opts, resErr, res, body, callback) {
-  var cacheFile;
-  var meta;
-  var cacheable = opts.cachePath && (! resErr) && res.headers &&
+  let cacheFile;
+  let meta;
+  const cacheable = opts.cachePath && (!resErr) && res.headers &&
       (opts.cacheAny || res.headers.etag);
 
   // if not cacheable return
-  if (! cacheable) {
+  if (!cacheable) {
     return callback();
   }
 
   // initialise the cache filename and metafile
   cacheFile = path.resolve(opts.cachePath, getCacheTarget(target));
-  meta = cacheFile + '.meta';
-  
+  meta = `${cacheFile}.meta`;
+
   // do the caching thing
-  mkdirp(opts.cachePath, function(err) {
+  mkdirp(opts.cachePath, (err) => {
     if (err) {
       return callback(err);
     }
 
     // create the metadata file
-    fs.writeFile(meta, JSON.stringify(res.headers), 'utf8', function() {
-      var match = reCharset.exec(res.headers['content-type']);
-      var encoding = match ? match[1] : 'utf8';
-      
-      fs.writeFile(cacheFile, body, encoding, function() {
+    fs.writeFile(meta, JSON.stringify(res.headers), 'utf8', () => {
+      const match = reCharset.exec(res.headers['content-type']);
+      const encoding = match ? match[1] : 'utf8';
+
+      fs.writeFile(cacheFile, body, encoding, () => {
         callback();
       });
     });
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 /* jshint node: true */
-'use strict';
 
-var debug = require('debug')('getit');
-var fs = require('fs');
-var path = require('path');
-var request = require('hyperquest');
-var urlish = require('urlish');
+
+const debug = require('debug')('getit');
+const fs = require('fs');
+const path = require('path');
+const request = require('hyperquest');
+const urlish = require('urlish');
 
 // regexes
-var reRemote = /^\w[\w\.\+\-]+\:\/\//;
-var reStatusCached = /^304$/;
-var reStatusOK = /^(2|3)\d+/;
-    
+const reRemote = /^\w[\w\.\+\-]+\:\/\//;
+const reStatusCached = /^304$/;
+const reStatusOK = /^(2|3)\d+/;
+
 // cache helpers
-var cache = require('./cache');
+const cache = require('./cache');
 
 /**
   # getit
 
-  This is a simple remote file loader that makes it easy to open both local 
+  This is a simple remote file loader that makes it easy to open both local
   and remote files in a simple (and consistent) way.  Behind the scenes getit
   uses [hyperquest](https://github.com/substack/hyperquest) module to to the
   heavy lifting.
@@ -29,7 +29,7 @@ var cache = require('./cache');
 
   ```js
   getit('files/test.txt', function(err, data) {
-      
+
   });
   ```
 
@@ -37,36 +37,36 @@ var cache = require('./cache');
 
   ```js
   getit('http://www.google.com/', function(err, data) {
-      
+
   });
   ```
 
   ### Specifying the Current Working Directory
 
-  By default, all files are resolved to the current working directory through 
+  By default, all files are resolved to the current working directory through
   using `path.resolve`. The default directory resolved against can be
   overriden, however, by passing options to the `getit` function call:
 
   ```js
   getit('files/test.txt', { cwd: __dirname }, function(err, data) {
-      
+
   });
   ```
 
-  Specifying the `cwd` option has no effect on remote requests, but there 
+  Specifying the `cwd` option has no effect on remote requests, but there
   might be other options added in time to tweak the default
-  hyperquest behaviour eventually.  The general principle is you should be 
+  hyperquest behaviour eventually.  The general principle is you should be
   able to use `getit` to get the content of both local and remote resources
   without having to dramatically change the way you use the library.
 
   ## GetIt Options
 
   The `getit` function supports a second argument for providing options to
-  change the default getit behaviour.  
+  change the default getit behaviour.
 
   ### Caching use `cachePath`
 
-  If you provide an optional `cachePath`, then getit will cache a copy of 
+  If you provide an optional `cachePath`, then getit will cache a copy of
   the data retrieved in the specified path.  In addition to the data
   retrieved, an [etag](http://en.wikipedia.org/wiki/HTTP_ETag) value will
   be stored in a lookup file.  This will be used in subsequent lookups
@@ -85,7 +85,7 @@ var cache = require('./cache');
     'github://DamonOehlman/getit/test/files/test.txt',
     opts,
     function(err, data) {
-      
+
     }
   );
   ```
@@ -106,23 +106,23 @@ var cache = require('./cache');
   local cache path.
 
 **/
-    
-var getit = module.exports = function(target, opts, callback) {
-  var targetUrl;
-  var requestOpts;
+
+const getit = module.exports = function(target, opts, callback) {
+  let targetUrl;
+  let requestOpts;
 
   // check for options being omitted
-  if (typeof opts == 'function') {
+  if (typeof opts === 'function') {
     callback = opts;
     opts = {};
   }
-  
+
   // initialise opts
   opts = opts || {};
   opts.cwd = opts.cwd || process.cwd();
 
   // if not a remote url, then get local
-  if (! isRemote(target)) {
+  if (!isRemote(target)) {
     return getLocal(target, opts, callback);
   }
 
@@ -132,21 +132,21 @@ var getit = module.exports = function(target, opts, callback) {
   // initialise the request opts
   requestOpts = {
     method: 'GET',
-    uri: targetUrl
+    uri: targetUrl,
   };
 
   // if we don't have a callback, return a request stream
-  if (! callback) {
-    debug('creating stream for retrieving: ' + targetUrl);
+  if (!callback) {
+    debug(`creating stream for retrieving: ${targetUrl}`);
     return request(requestOpts);
   }
 
   // check the cache
-  cache.get(target, opts, function(cacheData) {
+  cache.get(target, opts, (cacheData) => {
     // if we have cache data then add the if-none-match header
     if (cacheData.etag) {
       requestOpts.headers = {
-        'If-None-Match': cacheData.etag
+        'If-None-Match': cacheData.etag,
       };
     }
 
@@ -155,31 +155,31 @@ var getit = module.exports = function(target, opts, callback) {
       return callback(null, cacheData.data);
     }
 
-    debug('requesting resource: ' + targetUrl +', for target: ' + target);
+    debug(`requesting resource: ${targetUrl}, for target: ${target}`);
     request(requestOpts)
       // handle the response
-      .on('response', function(res) {
-        var body = '';
+      .on('response', (res) => {
+        let body = '';
 
-        debug('received response for target: ' + target);
+        debug(`received response for target: ${target}`);
         // if cached, then return the catched content
         if (reStatusCached.test(res.statusCode)) {
           return callback(null, cacheData.data, true);
         }
 
         // otherwise, if not ok, then return an error
-        if (! reStatusOK.test(res.statusCode)) {
-          return callback(new Error(((res.headers || {}).status ||
-            'Not found')) + ': ' + targetUrl);
+        if (!reStatusOK.test(res.statusCode)) {
+          return callback(`${new Error(((res.headers || {}).status ||
+            'Not found'))}: ${targetUrl}`);
         }
 
         // otherwise, proceed to download the content
-        res.on('data', function(buffer) {
+        res.on('data', (buffer) => {
           body += buffer.toString('utf8');
         });
 
-        res.on('end', function() {
-          cache.update(target, opts, null, res, body, function() {
+        res.on('end', () => {
+          cache.update(target, opts, null, res, body, () => {
             callback(null, body);
           });
         });
@@ -191,46 +191,45 @@ var getit = module.exports = function(target, opts, callback) {
 };
 
 var getLocal = getit.local = function(target, opts, callback) {
-  var targetFile = path.resolve(opts.cwd, target);
+  const targetFile = path.resolve(opts.cwd, target);
 
   // if we don't have a callback, return a stream
-  if (! callback) {
+  if (!callback) {
     return fs.createReadStream(targetFile);
   }
 
-  // if a callback is defined, then read the file using the 
-  debug('reading file: ' + targetFile);
-  fs.readFile(targetFile, 'utf8', function(err, data) {
-    debug('read file: ' + targetFile + ', err: ' + err);
+  // if a callback is defined, then read the file using the
+  debug(`reading file: ${targetFile}`);
+  fs.readFile(targetFile, 'utf8', (err, data) => {
+    debug(`read file: ${targetFile}, err: ${err}`);
 
     callback(err, data);
   });
-  
+
   return null;
 };
 
 var getUrl = getit.getUrl = function(target) {
-  var parts = urlish(target);
-  var translator;
+  const parts = urlish(target);
+  let translator;
 
   // if we have no parts, just return the target
-  if (! parts) {
+  if (!parts) {
     return target;
   }
-      
+
   // try and include the scheme translator
   try {
-    translator = require('./lib/schemes/' + parts.scheme);
-  }
-  catch (e) {
+    translator = require(`./lib/schemes/${parts.scheme}`);
+  } catch (e) {
     // no translator found, leave the handler blank
   }
-  
+
   // if we have a translator, then use it
   if (translator) {
     target = translator(parts, target);
   }
-  
+
   // return the target
   return target;
 };
